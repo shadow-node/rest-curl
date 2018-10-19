5e63f5c142db9ba0b036a458ba084aaa2bfd963c
chenximin <ximin.chen@rokid.com>
2018-10-19 21:01:39 +0800

    use napi

--- restcurl.cc
+++ restcurl.cc
@@ -3,9 +3,9 @@
 #include <list>
 extern "C"
 {
-  #include <uv.h>
-  #include <node_api.h>
-  #include "./common.h"
+#include <uv.h>
+#include <node_api.h>
+#include "./common.h"
 }
 
 #include "restclient-cpp/restclient.h"
@@ -13,72 +13,61 @@ extern "C"
 
 #define SERVER_URL "https://qqmusic-mobile-pro.rokid.com/rokid-cas-qqmusic/uniformEntry"
 
-struct RestCurlResponseData
+struct RestcurlData
 {
-  napi_env env;
   napi_ref cbRef;
+  napi_async_work work;
+
+  int timeout;
+
+  RestClient::HeaderFields reqHeaders;
+  std::string reqBody;
+  size_t reqSize;
+
   int code;
-  std::string body;
-  RestClient::HeaderFields headers;
+  RestClient::HeaderFields resHeaders;
+  std::string resBody;
 };
 
-static std::mutex mutex;
-static uv_async_t async;
-static std::list<RestCurlResponseData *> responseCbs;
-
-static void doRequest(char *body, size_t bodySize, napi_env env, napi_ref cbRef)
+static void doRequest(napi_env env, void *workData)
 {
-  RestCurlResponseData *response = new RestCurlResponseData();
-  RestClient::Response r = RestClient::post(SERVER_URL, "application/json", body);
-  response->env = env;
-  response->cbRef = cbRef;
-  response->code = r.code;
-  response->headers.swap(r.headers);
-  response->body.swap(r.body);
-  mutex.lock();
-  responseCbs.push_back(response);
-  mutex.unlock();
-  uv_async_send(&async);
-  free(body);
+  auto data = (RestcurlData *)workData;
+  RestClient::Connection conn("");
+  conn.AppendHeader("Content-Type", "application/json");
+  RestClient::Response r = conn.post(SERVER_URL, data->reqBody);
+  data->code = r.code;
+  data->resHeaders.swap(r.headers);
+  data->resBody.swap(r.body);
 }
 
-static void onRequestFinish(uv_async_t *handle)
+static void onRequestFinish(napi_env env, napi_status status, void *workData)
 {
-  std::list<RestCurlResponseData *> cbs;
-  mutex.lock();
-  cbs.swap(responseCbs);
-  mutex.unlock();
-
-  int code;
-  std::string *body;
-  napi_ref cbRef;
-  size_t argc = 2;
-  napi_env env;
-  napi_value cb;
-  napi_value recv;
-  napi_value resName;
-  napi_async_context ctx;
+  auto data = (RestcurlData *)workData;
+  int code = data->code;
+  std::string &body = data->resBody;
+  napi_ref cbRef = data->cbRef;
+  printf("aaa\n");
   napi_handle_scope scope;
+  NAPI_CALL_RETURN_VOID(env, napi_open_handle_scope(env, &scope));
+  napi_value cb;
+  NAPI_CALL_RETURN_VOID(env, napi_get_reference_value(env, cbRef, &cb));
+  napi_value resName;
+  NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, "restcurl", NAPI_AUTO_LENGTH, &resName));
+  napi_async_context ctx;
+  NAPI_CALL_RETURN_VOID(env, napi_async_init(env, cb, resName, &ctx));
+  napi_value recv;
+  NAPI_CALL_RETURN_VOID(env, napi_get_global(env, &recv));
+  size_t argc = 2;
   napi_value argv[argc];
-  for (auto ite = cbs.begin(); ite != cbs.end(); ++ite)
-  {
-    code = (*ite)->code;
-    body = &(*ite)->body;
-    env = (*ite)->env;
-    cbRef = (*ite)->cbRef;
-    NAPI_CALL_RETURN_VOID(env, napi_open_handle_scope(env, &scope));
-    NAPI_CALL_RETURN_VOID(env, napi_get_reference_value(env, cbRef, &cb));
-    NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, "restcurl", NAPI_AUTO_LENGTH, &resName));
-    NAPI_CALL_RETURN_VOID(env, napi_async_init(env, cb, resName, &ctx));
-    NAPI_CALL_RETURN_VOID(env, napi_get_global(env, &recv));
-    NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, code, &argv[0]));
-    NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, body->c_str(), body->size(), &argv[1]));
-    NAPI_CALL_RETURN_VOID(env, napi_make_callback(env, ctx, recv, cb, argc, argv, nullptr));
-    NAPI_CALL_RETURN_VOID(env, napi_delete_reference(env, cbRef));
-    NAPI_CALL_RETURN_VOID(env, napi_async_destroy(env, ctx));
-    NAPI_CALL_RETURN_VOID(env, napi_close_handle_scope(env, scope));
-    delete (*ite);
-  }
+  NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, code, &argv[0]));
+  NAPI_CALL_RETURN_VOID(env, napi_create_string_utf8(env, body.c_str(), body.size(), &argv[1]));
+  NAPI_CALL_RETURN_VOID(env, napi_make_callback(env, ctx, recv, cb, argc, argv, nullptr));
+  NAPI_CALL_RETURN_VOID(env, napi_delete_reference(env, cbRef));
+  NAPI_CALL_RETURN_VOID(env, napi_async_destroy(env, ctx));
+  NAPI_CALL_RETURN_VOID(env, napi_close_handle_scope(env, scope));
+  NAPI_CALL_RETURN_VOID(env, napi_delete_async_work(env, data->work));
+  free(data);
+  printf("bbb\n");
 }
 
 static napi_value request(napi_env env, napi_callback_info info)
@@ -99,25 +88,27 @@ static napi_value request(napi_env env, napi_callback_info info)
     NAPI_CALL(env, napi_throw_error(env, nullptr, "Argument 2 expected a function"));
     return nullptr;
   }
-  size_t bodySize;
-  NAPI_CALL(env, napi_get_value_string_utf8(env, argv[0], NULL, 0, &bodySize));
-  char *body = (char *)malloc(bodySize + 1);
-  NAPI_CALL(env, napi_get_value_string_utf8(env, argv[0], body, bodySize, &bodySize));
-  body[bodySize] = '\0';
-  napi_ref cbRef;
-  NAPI_CALL(env, napi_create_reference(env, cb, 1, &cbRef));
-  std::thread th(doRequest, body, bodySize, env, cbRef);
-  th.detach();
+  auto data = (RestcurlData *)malloc(sizeof(RestcurlData));
+  NAPI_CALL(env, napi_get_value_string_utf8(env, argv[0], NULL, 0, &data->reqSize));
+  char *reqBody = (char *)malloc(data->reqSize);
+  NAPI_CALL(env, napi_get_value_string_utf8(env, argv[0], reqBody, data->reqSize, &data->reqSize));
+  // assign const char * to std::string
+  data->reqBody.assign(reqBody, data->reqSize);
+  free(reqBody);
+  NAPI_CALL(env, napi_create_reference(env, cb, 1, &data->cbRef));
+  napi_value resName;
+  NAPI_CALL(env, napi_create_string_utf8(env, "restcurl", NAPI_AUTO_LENGTH, &resName));
+  NAPI_CALL(env, napi_create_async_work(env, cb, resName, doRequest, onRequestFinish, data, &data->work));
+  NAPI_CALL(env, napi_queue_async_work(env, data->work));
   return nullptr;
 }
 
 static napi_value Init(napi_env env, napi_value exports)
 {
-  uv_loop_s* loop;
+  uv_loop_s *loop;
   NAPI_CALL(env, napi_get_uv_event_loop(env, &loop));
-  uv_async_init(loop, &async, onRequestFinish);
   napi_property_descriptor desc[] = {
-    DECLARE_NAPI_PROPERTY("request", request),
+      DECLARE_NAPI_PROPERTY("request", request),
   };
   size_t property_count = sizeof(desc) / sizeof(*desc);
   NAPI_CALL(env, napi_define_properties(env, exports, property_count, desc));
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var restcurl = require('./build/Release/restcurl.node')
+var restcurl = require('./restcurl.node')
 
 var data = '{"locale":"zh-cn","domain":"com.rokid.cas.qqmusic","appVersion":"1.0.0","signMethod":"MD5","appId":"RBA66C902A6347DD86CA8D419B0BB974","masterId":"0504031838000016","deviceId":"0504031838000016","deviceType":"0ABA0AA4F71949C4A3FB0418BF025113","businessParams":{},"requestTimestamp":1539426109,"intent":"play_random","nonce":"0504031838000016153942610925","requestId":1539426109,"sessionId":1539426109,"sign":"DD0D8B21DCDAEE6076D338E59908BFF8"}'
 setInterval(() => {
