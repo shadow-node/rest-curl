c00b47a3ad9eb5a00ab08825c3e02b04015fde0f
chenximin <ximin.chen@rokid.com>
2018-10-19 16:06:27 +0800

    update demo

--- demo.cc
+++ demo.cc
@@ -2,25 +2,110 @@
 #include "restclient-cpp/connection.h"
 #include <iostream>
 #include <time.h>
+#include <uv.h>
+#include <list>
+#include <thread>
+#include <mutex>
 
-double jerry_port_get_current_time(void)
+#define SERVER_URL "https://qqmusic-mobile-pro.rokid.com/rokid-cas-qqmusic/uniformEntry"
+
+struct RestCurlResponseData
 {
-  struct timeval tv;
-  if (gettimeofday(&tv, NULL) != 0)
-  {
-    return 0.0;
-  }
-  return ((double)tv.tv_sec) * 1000.0 + ((double)tv.tv_usec) / 1000.0;
+  int code;
+  std::string body;
+  RestClient::HeaderFields headers;
+};
+
+static std::mutex mutex;
+static uv_async_t async;
+static uv_timer_t timer;
+static std::list<RestCurlResponseData *> responseCbs;
+
+static RestCurlResponseData *doRequest(char *body, size_t bodySize, void *env, void *ref)
+{
+  RestCurlResponseData *response = new RestCurlResponseData();
+  RestClient::Response r = RestClient::post(SERVER_URL, "application/json", body);
+  response->code = r.code;
+  response->headers.swap(r.headers);
+  response->body.swap(r.body);
+  free(body);
+  return response;
 }
 
-int main()
+static void doRequestAsync(char *body, size_t bodySize, void *env, void *ref)
+{
+  RestCurlResponseData *response = doRequest(body, bodySize, env, ref);
+  mutex.lock();
+  responseCbs.push_back(response);
+  mutex.unlock();
+  uv_async_send(&async);
+}
+
+static void onAsyncRequestFinish(uv_async_t *handle)
+{
+  std::list<RestCurlResponseData *> cbs;
+  mutex.lock();
+  cbs.swap(responseCbs);
+  mutex.unlock();
+
+  int code;
+  std::string *body;
+  size_t argc = 2;
+  for (auto ite = cbs.begin(); ite != cbs.end(); ++ite)
+  {
+    code = (*ite)->code;
+    body = &(*ite)->body;
+    std::cout << "code: " << code << std::endl;
+    std::cout << "body: " << *body << std::endl;
+    delete (*ite);
+  }
+}
+
+void onTimer(uv_timer_t *handle)
+{
+  size_t bodySize = 2;
+  char *body = (char *)malloc(bodySize + 1);
+  memcpy(body, "{}", bodySize);
+  body[bodySize] = '\0';
+  std::thread th(doRequestAsync, body, bodySize, nullptr, nullptr);
+  th.detach();
+}
+
+int syncMain()
 {
   while (true)
   {
-    double start = jerry_port_get_current_time();
-    RestClient::Response r = RestClient::get("https://www.baidu.com");
-    double end = jerry_port_get_current_time();
-    std::cout << "code:" << r.code << ", time:" << end - start << "ms" << std::endl;
+    size_t bodySize = 2;
+    char *body = (char *)malloc(bodySize + 1);
+    memcpy(body, "{}", bodySize);
+    body[bodySize] = '\0';
+    RestCurlResponseData *response = doRequest(body, bodySize, nullptr, nullptr);
+    std::cout << "code: " << response->code << std::endl;
+    std::cout << "body: " << response->body << std::endl;
+    delete response;
   }
   return 0;
+}
+
+int asyncMain()
+{
+  uv_async_init(uv_default_loop(), &async, onAsyncRequestFinish);
+  uv_timer_init(uv_default_loop(), &timer);
+  uv_timer_start(&timer, onTimer, 0, 200);
+  uv_run(uv_default_loop(), UV_RUN_DEFAULT);
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc == 2 && strcmp(argv[1], "async") == 0)
+  {
+    std::cout << "request with async" << std::endl;
+    return asyncMain();
+  }
+  else
+  {
+    std::cout << "request with sync" << std::endl;
+    return syncMain();
+  }
 }
